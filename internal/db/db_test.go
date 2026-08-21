96c8f44c53522d0af41cf29b716588e674167850
David Norris <>
2026-08-21 12:10:51 +0800

    Update test to include new `analysis_catalog` table in expected database schema

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -28,7 +28,7 @@ func TestOpenCreatesAppTables(t *testing.T) {
 
 	want := []string{
 		"users", "series", "stories", "story_acts", "story_documents", "character_profiles",
-		"series_bible_documents", "document_types", "user_settings", "app_settings", "disk_hashes",
+		"series_bible_documents", "document_types", "analysis_catalog", "user_settings", "app_settings", "disk_hashes",
 		"analysis_reports", "analysis_results",
 	}
 	for _, table := range want {
